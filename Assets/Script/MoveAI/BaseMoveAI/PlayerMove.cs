a97d23fee6007b5212052f8d1fb915ebeaf6dc67
ДимДим <1000rub@list.ru>
2024-03-20 20:51:38 +0300

    200324

--- Assets/Script/MoveAI/BaseMoveAI/PlayerMove.cs
+++ Assets/Script/MoveAI/BaseMoveAI/PlayerMove.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     private MoveAI moveAI;
+
+    [SerializeField][Range(0, 100)] private float speedForward = 100f;
+    [SerializeField][Range(0, 100)] private float stopDistance = 2f;
+    [SerializeField] private Transform target;
+    private Rigidbody thisRigibody;
+
+    private bool isStopClass = false, isRun = false;
+
     void Start()
     {
-        moveAI = new MoveAI(this.gameObject);
+        SetClass();
+    }
+    private void SetClass()
+    {
+        isRun = true;
+        thisRigibody = GetComponent<Rigidbody>();
+        SettingMoveAI settingMoveAI = new SettingMoveAI
+        {
+            GameObject = this.gameObject,
+            Rigidbody = thisRigibody,
+            MoveSpeed = speedForward,
+            StopDistance = stopDistance,
+        };
+        moveAI = new MoveAI(settingMoveAI);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isStopClass) { return; }
+        if (!isRun) { SetClass(); }
+        RunUpdate();
+    }
+    private void RunUpdate()
+    {
+        moveAI.UpdateMove(target.position);
     }
 }
--- Assets/Script/MoveAI/MoveAI/MoveAI.cs
+++ Assets/Script/MoveAI/MoveAI/MoveAI.cs
@@ -1,59 +1,82 @@
 using UnityEngine;
 
+public struct SettingMoveAI
+{
+    public GameObject GameObject;
+    public Rigidbody Rigidbody;
+    public float MoveSpeed;
+    public float RotationSpeed;
+    public float StopDistance;
+}
+
 public class MoveAI
 {
-    private GameObject gameObject;
-    private Transform target;
-    private float speedRotation;
-    private float speedMove;
+    private SettingMoveAI settingMoveAI;
+    private Vector3 moveDirection, targetPosition;
+    private float distance;
+    private Quaternion deltaRotation, directionRotation;
     //
-    public Quaternion rotationGameObject;
-    public Vector3 positionGameObject;
-    public Quaternion rotationTarget;
-    public Vector3 positionTarget, forward;
+    private bool isTriggerRunMove=true;
     //
-    private Quaternion lookRotationTarget;
-    private Vector3 direction;
-    public MoveAI(GameObject _gameObject)
+    public MoveAI(SettingMoveAI _settingMoveAI)
     {
-        gameObject = _gameObject;
-        rotationGameObject = _gameObject.transform.rotation;
-        positionGameObject = _gameObject.transform.position;
+        settingMoveAI = _settingMoveAI;
+        if (settingMoveAI.StopDistance == 0) { settingMoveAI.StopDistance = 1f; }
+        if (settingMoveAI.MoveSpeed == 0) { settingMoveAI.MoveSpeed = 1f; }
+        if (settingMoveAI.RotationSpeed == 0) { settingMoveAI.RotationSpeed = 1f; }
     }
-    public void SetSpeed(float _speedMove = 1, float _speedRotation = 1)
+    public void UpdateMove(Vector3 target, bool isOffAxisY = true)
     {
-        speedMove = _speedMove ;
-        speedRotation = _speedRotation ;
+        targetPosition = target;
+        MoveInTargetLine(isOffAxisY);
     }
-    public void SetTarget(Transform _target)
+    private Vector3 MoveDirection()
     {
-        target = _target;
-        rotationTarget = target.rotation;
-        positionTarget = target.position;
+        return targetPosition - settingMoveAI.GameObject.transform.position;
     }
-    private void UpDateTransform()
+    private Quaternion GetRotation(Vector3 moveDirection, bool isOffAxisY)
     {
-        positionTarget = target.position;
-        rotationTarget = target.rotation;
-        positionGameObject = gameObject.transform.position;
-        rotationGameObject = gameObject.transform.rotation;
-        direction = positionTarget - positionGameObject;
-    }
-    public Quaternion GetRotation(bool isOnAxisY = true)
-    {
-        UpDateTransform();
-        lookRotationTarget = Quaternion.LookRotation(direction);
-        rotationTarget = Quaternion.Lerp(rotationGameObject,
-                                        lookRotationTarget,
-                                        speedRotation * Time.fixedDeltaTime);
+        if (isOffAxisY) { moveDirection.y = 0; }
 
-        if (!isOnAxisY) { rotationTarget.x = 0; }
-        return rotationTarget;
+        deltaRotation = Quaternion.LookRotation(moveDirection);
+        directionRotation = Quaternion.Lerp(settingMoveAI.GameObject.transform.rotation,
+                                            deltaRotation,
+                                            settingMoveAI.RotationSpeed);
+        return directionRotation;
     }
-    public Vector3 GetMove(bool isOnAxisY = true)
+    private void MoveInTargetLine(bool isOffAxisY)
     {
-        forward = gameObject.transform.forward;
-        if (!isOnAxisY) { forward.y = 0f; }
-        return forward * speedMove ;
+        if (isOffAxisY) { distance = Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z); }
+        else { distance = Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z) + Mathf.Abs(moveDirection.y); }
+
+        moveDirection = MoveDirection();
+
+        if (distance <= settingMoveAI.StopDistance)
+        {
+            StopMove(moveDirection, isOffAxisY);
+        }
+        else
+        {
+            RunMove(moveDirection, isOffAxisY);
+        }
+
+        if(distance > settingMoveAI.StopDistance * 1.1f) { isTriggerRunMove=true; }
+    }
+    private void StopMove(Vector3 moveDirection,bool isOffAxisY)
+    {
+        if (isTriggerRunMove)
+        {
+            isTriggerRunMove = !isTriggerRunMove;
+            settingMoveAI.Rigidbody.velocity = Vector3.zero;
+            settingMoveAI.Rigidbody.MoveRotation(GetRotation(moveDirection, isOffAxisY));
+        }
+    }
+    private void RunMove(Vector3 moveDirection, bool isOffAxisY)
+    {
+        if (isTriggerRunMove)
+        {
+            settingMoveAI.Rigidbody.velocity = settingMoveAI.Rigidbody.transform.forward * settingMoveAI.MoveSpeed;
+            settingMoveAI.Rigidbody.MoveRotation(GetRotation(moveDirection, isOffAxisY));
+        }
     }
 }
